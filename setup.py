60a03eecf35201267f9dd038e3d4bd6aa3ac1bba
KemuelRocha <kemuelsr@gmail.com>
2023-01-31 13:45:29 -0300

    setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 with open("README.md", "r") as arq:
     readme = arq.read()
 
-setup(name='digital-signer',
+setup(name='signer-icpedu',
     version='0.0.1',
     license='MIT License',
     author='Kemuel dos Santos Rocha',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='kemuel.rocha@discente.univasf.edu.br',
-    keywords='digital signer icpedu',
+    keywords='signer icpedu',
     description=u'Package para assinaturas digitais',
     packages=['signer_icpedu'],
     install_requires=["cryptography", "endesive", "pyOpenSSL"])
